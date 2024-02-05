a144ebf548f3485898fedc7365509e8b06e8fdce
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-05 23:36:44 +0800

    Add project files.

---
+++ AssignmentDeclareDeconstruction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace AssignmentDeclareDeconstruction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = (Name: "Greg Esguerra", Address: "Cebu City Philippines");
+            //deconstruct
+            (string name, string address) = student;
+            Console.WriteLine(name);
+            Console.WriteLine(address);
+
+            //C# 10 version 
+            Console.WriteLine("\nC# 10 version");
+            string nameOne;
+            (nameOne, string addressOne) = student;
+            Console.WriteLine(nameOne);
+            Console.WriteLine(addressOne);
+
+            Console.ReadLine();
+        }
+    }
+}
